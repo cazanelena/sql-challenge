fe0a25fb6810189d7fb85978011e5da54b832866
Elena Cazan <59057287+cazanelena@users.noreply.github.com>
2023-09-25 13:57:16 +0100

    challenges solved

--- model.js
+++ model.js
@@ -1,15 +1,20 @@
 const db = require("./database/db.js");
 
 const select_cohorts_in_finsbo = db.prepare(/*sql*/ `
-  -- [1]
+  SELECT name FROM cohorts WHERE location = 'Finsbury Park' 
 `);
 
 function listCohortsInFinsbo() {
   return select_cohorts_in_finsbo.all();
 }
 
+
 const select_students_in_finsbo = db.prepare(/*sql*/ `
-  -- [2]
+  SELECT username 
+  FROM students
+  JOIN cohorts ON students.cohort_name = cohorts.name
+  WHERE location = 'Finsbury Park'
+
 `);
 
 function listStudentsInFinsbo() {
@@ -17,15 +22,18 @@ function listStudentsInFinsbo() {
 }
 
 const select_students_with_location = db.prepare(/*sql*/ `
-  -- [3]
+  SELECT username, location FROM students
+  JOIN cohorts ON students.cohort_name = cohorts.name
 `);
 
 function listStudentsWithLocation() {
   return select_students_with_location.all();
 }
 
+
 const select_students_with_projects = db.prepare(/*sql*/ `
-  -- [4]
+  SELECT projects.name, students_projects.student_username AS username FROM projects
+  JOIN students_projects ON students_projects.project_id = projects.id
 `);
 
 function listStudentsWithProjects() {
@@ -33,12 +41,19 @@ function listStudentsWithProjects() {
 }
 
 const select_students_with_projects_in_finsbo = db.prepare(/*sql*/ `
-  -- [5]
+  SELECT projects.name, students_projects.student_username AS username FROM projects
+  JOIN students_projects ON students_projects.project_id = projects.id
+  JOIN students ON students.username = students_projects.student_username
+  JOIN cohorts ON cohorts.name = students.cohort_name
+  WHERE location = 'Finsbury Park'
+  ORDER BY username
 `);
 
 function listStudentsWithProjectsInFinsbo() {
   return select_students_with_projects_in_finsbo.all();
 }
+
+console.log(listStudentsWithProjectsInFinsbo())
 
 module.exports = {
   listCohortsInFinsbo,
